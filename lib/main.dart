2f7811f5c8e165fab9a98b632fc7d39efa9e1035
Victor <victor.araujo@grupoboticario.com.br>
2022-10-21 10:06:42 -0300

    feat: add new field

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,7 +35,13 @@ class CreateEventPage extends StatefulWidget {
 
 class EventDetails {
   late String title;
-  late String description;
+  late String startDate;
+  late String endDate;
+  late String startTime;
+  late String endTime;
+  String? description;
+  String? address;
+  String? site;
 
   @override
   String toString() {
@@ -44,6 +52,12 @@ class EventDetails {
     return {
       'title': title,
       'description': description,
+      'startDate': startDate,
+      'endDate': endDate,
+      'startTime': startTime,
+      'endTime': endTime,
+      'address': address,
+      'site': site,
     };
   }
 }
@@ -53,6 +67,15 @@ class _CreateEventPageState extends State<CreateEventPage> {
 
   final _eventTitleController = TextEditingController();
   final _eventDescriptionController = TextEditingController();
+  final _eventStartDateController = TextEditingController();
+  final _eventEndDateController = TextEditingController();
+  final _eventStartTimeController = TextEditingController();
+  final _eventEndTimeController = TextEditingController();
+  final _eventAddressController = TextEditingController();
+  final _eventSiteController = TextEditingController();
+
+  final dateMask = MaskTextInputFormatter(mask: "##/##/####");
+  final timeMask = MaskTextInputFormatter(mask: "##:##");
 
   EventDetails eventDetails = EventDetails();
 
@@ -62,60 +85,87 @@ class _CreateEventPageState extends State<CreateEventPage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  TextField(
-                    label: 'Título',
-                    controller: _eventTitleController,
-                  ),
-                  TextField(
-                    label: 'Descrição',
-                    controller: _eventDescriptionController,
-                  ),
-                  ElevatedButton(
-                    onPressed: () async {
-                      if (_formKey.currentState!.validate()) {
-                        eventDetails.title = _eventTitleController.text;
-                        eventDetails.description =
-                            _eventDescriptionController.text;
+      body: SingleChildScrollView(
+        child:
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      label: 'Título',
+                      controller: _eventTitleController,
+                    ),
+                    TextField(
+                      label: 'Descrição',
+                      controller: _eventDescriptionController,
+                    ),
+                    TextField(
+                      label: 'Data de início',
+                      controller: _eventStartDateController,
+                      inputFormatters: [dateMask],
+                      keyboardType: TextInputType.datetime,
+                    ),
+                    TextField(
+                      label: 'Data de fim',
+                      controller: _eventEndDateController,
+                      inputFormatters: [dateMask],
+                      keyboardType: TextInputType.datetime,
+                    ),
+                    TextField(
+                      label: 'Horário de início',
+                      controller: _eventStartTimeController,
+                      inputFormatters: [timeMask],
+                      keyboardType: TextInputType.datetime,
+                    ),
+                    TextField(
+                      label: 'Horário de fim',
+                      controller: _eventEndTimeController,
+                      inputFormatters: [timeMask],
+                      keyboardType: TextInputType.datetime,
+                    ),
+                    TextField(
+                      label: 'Endereço',
+                      controller: _eventAddressController,
+                    ),
+                    TextField(
+                      label: 'Site',
+                      controller: _eventSiteController,
+                    ),
+                    ElevatedButton(
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          eventDetails.title = _eventTitleController.text;
+                          eventDetails.description =
+                              _eventDescriptionController.text;
+                          eventDetails.startDate = _eventStartDateController.text;
+                          eventDetails.endDate = _eventEndDateController.text;
+                          eventDetails.startTime = _eventStartTimeController.text;
+                          eventDetails.endTime = _eventEndTimeController.text;
+                          eventDetails.address = _eventAddressController.text;
+                          eventDetails.site = _eventSiteController.text;
 
-                        var url =
-                            Uri.https('6464-45-174-146-51.sa.ngrok.io', '/');
-                        var response = await http.post(
-                          url,
-                          headers: {"content-type": "application/json"},
-                          body: jsonEncode(eventDetails),
-                        );
+                          var url =
+                              Uri.https('5ed9-45-174-146-51.sa.ngrok.io', '/');
+                          var response = await http.post(
+                            url,
+                            headers: {"content-type": "application/json"},
+                            body: jsonEncode(eventDetails),
+                          );
 
-                        print('Response status: ${response.statusCode}');
-                        print('Response body: ${response.body}');
-                      }
-                    },
-                    child: const Text('Submit'),
-                  ),
-                  // InputDatePickerFormField(
-                  //   firstDate: DateTime(DateTime.now().year - 120),
-                  //   lastDate: DateTime.now(),
-                  //   fieldHintText: 'mm/dd/yyyy',
-                  //   fieldLabelText: 'Data de início',
-                  // ),
-                  // InputDatePickerFormField(
-                  //   firstDate: DateTime(DateTime.now().year - 120),
-                  //   lastDate: DateTime.now(),
-                  //   fieldHintText: 'mm/dd/yyyy',
-                  //   fieldLabelText: 'Data de fim',
-                  // ),
-                ],
+                          print('Response status: ${response.statusCode}');
+                          print('Response body: ${response.body}');
+                        }
+                      },
+                      child: const Text('Submit'),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
-        ),
+            ],
+          ),
       ),
     );
   }
@@ -124,14 +174,18 @@ class _CreateEventPageState extends State<CreateEventPage> {
 class TextField extends StatelessWidget {
   final String label;
   final TextEditingController controller;
+  final List<TextInputFormatter>? inputFormatters;
+  final TextInputType? keyboardType;
 
-  const TextField({super.key, required this.label, required this.controller});
+  const TextField({super.key, required this.label, required this.controller, this.inputFormatters, this.keyboardType});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(10),
       child: TextFormField(
+        keyboardType: keyboardType,
+        inputFormatters: inputFormatters,
         controller: controller,
         decoration: InputDecoration(
           labelText: label,
